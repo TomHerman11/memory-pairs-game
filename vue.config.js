2bb0d123a59e5f378be05f39c588b68112713ec5
Tom Herman <tomherman11@gmail.com>
2020-12-14 21:09:44 +0200

    follow official deploy documentation

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/memory-pairs-game/'
+    : '/'
 }
+
+// 'memory-pairs-game' is the name of the Github repository, see:
+// https://cli.vuejs.org/guide/deployment.html#github-pages
